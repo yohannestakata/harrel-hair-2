3504be5b1523eb2a50f6f85a2676780c4902092e
Yohannes Takata <yohannestakata1@gmail.com>
2025-03-21 17:32:23 +0300

    search on header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,25 +1,140 @@
-import {Suspense} from 'react';
-import {Await, NavLink, useAsyncValue} from '@remix-run/react';
+import {Suspense, useState, useId} from 'react';
+import {Await, NavLink, useAsyncValue, Link} from '@remix-run/react';
 import {useAnalytics, useOptimisticCart} from '@shopify/hydrogen';
 import {useAside} from '~/components/Aside';
+import {Search, ShoppingBag} from 'lucide-react';
+import logo from '~/assets/Logo-43.svg';
+import {
+  SEARCH_ENDPOINT,
+  SearchFormPredictive,
+} from '~/components/SearchFormPredictive';
+import {SearchResultsPredictive} from '~/components/SearchResultsPredictive';
 
 /**
  * @param {HeaderProps}
  */
 export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
   const {shop, menu} = header;
+  const queriesDatalistId = useId();
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
   return (
     <header className="header z-50 relative">
-      <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
-        <strong>{shop.name}</strong>
-      </NavLink>
-      <HeaderMenu
-        menu={menu}
-        viewport="desktop"
-        primaryDomainUrl={header.shop.primaryDomain.url}
-        publicStoreDomain={publicStoreDomain}
-      />
-      <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
+      <div className="max-w-7xl mx-auto flex items-center px-4 w-full">
+        <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
+          <div className="h-full overflow-hidden">
+            <img src={logo} alt="Harrel Hair" className="w-32" />
+          </div>
+        </NavLink>
+
+        <HeaderMenu
+          menu={menu}
+          viewport="desktop"
+          primaryDomainUrl={header.shop.primaryDomain.url}
+          publicStoreDomain={publicStoreDomain}
+        />
+
+        <div className="flex items-center gap-6 mx-auto ">
+          <div className="relative">
+            <SearchFormPredictive>
+              {({fetchResults, goToSearch, inputRef}) => (
+                <>
+                  <input
+                    autoComplete="off"
+                    name="q"
+                    onChange={fetchResults}
+                    onFocus={() => setIsSearchOpen(true)}
+                    onBlur={() => setTimeout(() => setIsSearchOpen(false), 200)}
+                    placeholder="Search..."
+                    ref={inputRef}
+                    type="text"
+                    list={queriesDatalistId}
+                    className="border border-zinc-400 px-4 w-xs rounded-sm h-10"
+                  />
+                  <button
+                    onClick={goToSearch}
+                    className="absolute right-2 top-2"
+                  >
+                    <Search size={24} color="#52525c" />
+                  </button>
+                </>
+              )}
+            </SearchFormPredictive>
+
+            {isSearchOpen && (
+              <div className="absolute top-full left-0 right-0 bg-white border border-gray-300 rounded-lg mt-1 z-10 shadow-lg p-4">
+                <SearchResultsPredictive>
+                  {({items, total, term, state, closeSearch}) => {
+                    const {articles, collections, pages, products, queries} =
+                      items;
+
+                    if (state === 'loading' && term.current) {
+                      return <div className="p-4">Loading...</div>;
+                    }
+
+                    if (!total) {
+                      return (
+                        <SearchResultsPredictive.Empty
+                          term={term}
+                          className="p-4"
+                        />
+                      );
+                    }
+
+                    return (
+                      <>
+                        <SearchResultsPredictive.Queries
+                          queries={queries}
+                          queriesDatalistId={queriesDatalistId}
+                          className="p-4"
+                        />
+                        <SearchResultsPredictive.Products
+                          products={products}
+                          closeSearch={closeSearch}
+                          term={term}
+                          className="p-4"
+                        />
+                        <SearchResultsPredictive.Collections
+                          collections={collections}
+                          closeSearch={closeSearch}
+                          term={term}
+                          className="p-4"
+                        />
+                        <SearchResultsPredictive.Pages
+                          pages={pages}
+                          closeSearch={closeSearch}
+                          term={term}
+                          className="p-4"
+                        />
+                        <SearchResultsPredictive.Articles
+                          articles={articles}
+                          closeSearch={closeSearch}
+                          term={term}
+                          className="p-4"
+                        />
+                        {term.current && total ? (
+                          <Link
+                            onClick={closeSearch}
+                            to={`${SEARCH_ENDPOINT}?q=${term.current}`}
+                            className="block p-4 border-t border-gray-200 hover:bg-gray-100"
+                          >
+                            <p>
+                              View all results for <q>{term.current}</q>
+                              &nbsp; →
+                            </p>
+                          </Link>
+                        ) : null}
+                      </>
+                    );
+                  }}
+                </SearchResultsPredictive>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
+      </div>
     </header>
   );
 }
@@ -57,7 +172,7 @@ export function HeaderMenu({
       {(menu || FALLBACK_HEADER_MENU).items.map((item) => {
         if (!item.url) return null;
 
-        // if the url is internal, we strip the domain
+        // If the URL is internal, strip the domain
         const url =
           item.url.includes('myshopify.com') ||
           item.url.includes(publicStoreDomain) ||
@@ -87,7 +202,7 @@ export function HeaderMenu({
  */
 function HeaderCtas({isLoggedIn, cart}) {
   return (
-    <nav className="header-ctas" role="navigation">
+    <nav className="flex items-center gap-6" role="navigation">
       <HeaderMenuMobileToggle />
       <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
         <Suspense fallback="Sign in">
@@ -96,7 +211,6 @@ function HeaderCtas({isLoggedIn, cart}) {
           </Await>
         </Suspense>
       </NavLink>
-      <SearchToggle />
       <CartToggle cart={cart} />
     </nav>
   );
@@ -110,15 +224,6 @@ function HeaderMenuMobileToggle() {
       onClick={() => open('mobile')}
     >
       <h3>☰</h3>
-    </button>
-  );
-}
-
-function SearchToggle() {
-  const {open} = useAside();
-  return (
-    <button className="reset" onClick={() => open('search')}>
-      Search
     </button>
   );
 }
@@ -143,8 +248,12 @@ function CartBadge({count}) {
           url: window.location.href || '',
         });
       }}
+      className="relative inline-block"
     >
-      Cart {count === null ? <span>&nbsp;</span> : count}
+      <ShoppingBag />
+      <div className="absolute -bottom-2 -right-2 text-sm bg-primary size-5 items-center justify-center  text-zinc-50 flex  rounded-full">
+        {count === null ? <span>&nbsp;</span> : count}
+      </div>
     </a>
   );
 }
